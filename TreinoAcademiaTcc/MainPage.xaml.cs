d90cc4932ac48898fb143a1f2d8e4c2696c5a374
btase <btase@BTA-EDUARDO>
2024-07-11 12:22:08 -0300

    Add project files.

---
+++ TreinoAcademiaTcc/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace TreinoAcademiaTcc
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
